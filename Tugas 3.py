defb00a49ee27f978a1c6c6f3a818ffd398588fd
rizqikaazzahra0807-dot <rizqikaazzahra0807@gmail.com>
2026-04-18 15:15:44 +0800

    Add files via upload

---
+++ Tugas 3.py
@@ -0,0 +1,9 @@
+while True:
+    angka = float(input("Masukkan angka: "))
+    
+    if angka >= 0:
+        break
+    else:
+        print("Harus positif!")
+
+print(f"Terima kasih! Kamu memasukkan angka: {angka}")
